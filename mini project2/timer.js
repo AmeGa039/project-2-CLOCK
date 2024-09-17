55140fa8566db53464d33b0dd8f3df58d5834c7e
AmeGa039 <eltionkrasniqi6@gmail.com>
2024-09-17 14:42:45 +0200

    Add files via upload

---
+++ mini project2/timer.js
@@ -0,0 +1,30 @@
+let timer;
+        let hours = 0;
+        let minutes = 0;
+        let seconds = 0;
+      
+        function displayTime() {
+            document.getElementById("clock").textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }
+      
+        function startStopwatch() {
+            timer = setInterval(() => {
+                seconds++;
+                if (seconds === 60) {
+                    seconds = 0;
+                    minutes++;
+                }
+                if (minutes === 60) {
+                    minutes = 0;
+                    hours++;
+                }
+                displayTime();
+            }, 1000);
+        }
+      
+        function stopStopwatch() {
+            clearInterval(timer);
+        }
+      
+        document.getElementById("startBtn").addEventListener("click", startStopwatch);
+        document.getElementById("stopBtn").addEventListener("click", stopStopwatch);
